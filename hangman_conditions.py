43467ab7b16322d2a6b9559f4ec29d1f9d3268e4
Sayan Basu <81770159+sayanbasu-cs@users.noreply.github.com>
2021-08-29 23:50:00 +0530

    Update in hangman drawings

--- hangman_conditions.py
+++ hangman_conditions.py
@@ -1,3 +1,4 @@
+# to check if the letter entered by player is a single letter or not
 def single_letter_check(c):
     if len(c) == 1:
         return c
@@ -5,6 +6,7 @@ def single_letter_check(c):
     return single_letter_check(c)
 
 
+# list specifying the stages a hangman is drawn on each wrong guess 
 def stages(stage):
     stages = [  """
                    --------
